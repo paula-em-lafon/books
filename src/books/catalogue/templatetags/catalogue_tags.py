8611d9f1b1f42fe5c80ae60847a4bd305d878ed9
paula-em-lafon <paula.em.lafon@gmail.com>
2016-09-23 15:15:46 -0500

    métodos de borrado y vistas listas

---
+++ src/books/catalogue/templatetags/catalogue_tags.py
@@ -0,0 +1,9 @@
+from django import template
+from books.catalogue.models import Libros
+register = template.Library()
+
+
+@register.simple_tag
+def get_libro(pk, attr):
+	obj = getattr(Libros.objects.get(isbn=pk), attr)
+	return obj
--- src/books/catalogue/urls.py
+++ src/books/catalogue/urls.py
@@ -1,8 +1,16 @@
 from django.conf.urls import include, url
 
-from .views import CreateAutorView, CreateEditorialView, CreateLibroView
+from .import views
+
 urlpatterns = [
-    url(r'^autores/nuevo/', CreateAutorView.as_view()),
-    url(r'^editoriales/nuevo/', CreateEditorialView.as_view()),
-    url(r'^libros/nuevo/', CreateLibroView.as_view())
+	url(r'^$', views.LibrosListView.as_view()),
+	url(r'^libros/nuevo/$', views.CreateLibroView.as_view()),
+	url(r'^libros/delete/$', views.delete_libros_view),
+	url(r'^autores/$', views.AutoresListView.as_view()),
+	url(r'^autores/nuevo/$', views.CreateAutorView.as_view()),
+	url(r'^autores/delete/$', views.delete_autores_view),
+	url(r'^editoriales/$', views.EditorialesListView.as_view()),
+	url(r'^editoriales/nuevo/$', views.CreateEditorialView.as_view()),
+	url(r'^editoriales/delete/$', views.delete_editoriales_view),
+
 ]
